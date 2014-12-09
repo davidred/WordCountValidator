51fb6dfb4c7c7637bf53e17ee9ffd7da9efb84fe
David Rozenberg <daveroze@gmail.com>
2014-12-09 12:11:33 -0500

    rspec test for text and list of excluded words

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,7 +6,7 @@ describe 'The Word Counting App' do
     Sinatra::Application
   end
 
-  
+
 
   context "When receiving a get request it" do
 
@@ -18,9 +18,17 @@ describe 'The Word Counting App' do
       expect(parsed_response).to have_key("exclude")
     end
 
-    it "returns a response that includes text"
+    it "returns a response that includes text" do
+      get '/'
+      parsed_response = JSON.parse(last_response.body)
+      expect(parsed_response['text']).to match(/\w+/)
+    end
 
-    it "returns a response that includes a list of excluded words"
+    it "returns a response that includes a list of excluded words" do
+      get '/'
+      parsed_response = JSON.parse(last_response.body)
+      expect(parsed_response['exclude']).to be_a(Array)
+    end
 
     it "returns an empty list if the text has only one unique word"
   end
